6c01cc3e81f6b284d42b37984f3a50398de5ea75
kesuuyof <kesuuyof@gmail.com>
2024-01-17 19:56:53 +0900

    fix ecs key and plus

--- keyboards/ai03/equinox/keymaps/default/keymap.c
+++ keyboards/ai03/equinox/keymaps/default/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [2] = LAYOUT( /* Num and FN */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-    KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   _______, KC_COLN, KC_MINS, _______,
-    _______, KC_F8,   KC_F9,   KC_F10,  KC_LSFT, KC_ESC,  _______, _______, _______, _______, _______, _______,
+    KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   _______, KC_COLN, KC_MINS, KC_PLUS,
+    _______, KC_F8,   KC_F9,   KC_F10,  KC_LSFT, KC_ESC,  _______, _______, _______, _______, _______, KC_ASTR,
     _______, _______, _______,          KC_LGUI,          _______,     _______,      _______, _______, QK_BOOT
   ),
   [3] = LAYOUT( /* Num and FN */

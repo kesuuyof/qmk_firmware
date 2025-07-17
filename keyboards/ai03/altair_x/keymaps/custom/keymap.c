0c19bc641e35a9c9c1d1b1bfb9a469db98193e7d
kesuuyof <kesuuyof@gmail.com>
2025-07-17 16:58:18 +0900

    Update Altair X keymap: Add KC_GRV to layer 0 and replace KC_SPC with KC_RSFT in layer 0

--- keyboards/ai03/altair_x/keymaps/custom/keymap.c
+++ keyboards/ai03/altair_x/keymaps/custom/keymap.c
@@ -6,10 +6,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_GRV , XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                                   KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_RALT, KC_DEL
+                                   KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_RSFT,  MO(2),   KC_RALT, KC_DEL
     ),
 
     [1] = LAYOUT(
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT(
         _______, _______, _______, KC_PGUP, _______, _______, _______, KC_HOME, KC_PGUP, _______, KC_UP,   KC_LBRC, KC_RBRC, KC_MINS,
         _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, KC_END , KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_RSFT,
                                    _______, _______, _______, _______, _______, _______, _______, KC_ESC
     )
 };
